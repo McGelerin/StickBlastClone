24f6cb5a7b8108a6f24ada6be2b01c5f602d5f4c
CAN <cangelerin@teekgames.com>
2025-05-01 20:04:12 +0300

    Level Creator Edited

--- StickBlastClone/Assets/Scripts/Runtime/GameArea/LevelArea/LevelCreator.cs
+++ StickBlastClone/Assets/Scripts/Runtime/GameArea/LevelArea/LevelCreator.cs
@@ -1,6 +1,8 @@
 using Runtime.Data.Persistent.Level;
 using Runtime.GameArea.Spawn;
 using Runtime.Grid;
+using Runtime.GridChecker.Signals;
+using Runtime.Identifiers;
 using Runtime.Infrastructures.Template;
 using Runtime.Models;
 using Runtime.Signals;
@@ -27,8 +29,52 @@ namespace Runtime.GameArea.LevelArea
         private void CreateLevelArea()
         {
             _signalBus.Fire(new SpawnObjectSignal());
-            int level = _gameProgressModel.Level % _levelsContainer.LevelData.Count;
+
+            SetupLevelLayoutOnGrid();
+            
             _signalBus.Fire(new ChangeLoadingScreenActivationSignal(isActive: false, null));
+        }
+        
+        private void SetupLevelLayoutOnGrid()
+        {
+            int level = _gameProgressModel.Level % _levelsContainer.LevelData.Count;
+            var gridData = _levelsContainer.LevelData[level].CustomCellDrawing;
+
+            for (int x = 0; x < gridData.GetLength(0); x++)
+            {
+                for (int y = 0; y < gridData.GetLength(1); y++)
+                {
+                    if (gridData[x,y])
+                    {
+                        var transposeX = gridData.GetLength(0) - 1 - x;
+                        var transposeY = gridData.GetLength(1) - 1 - y;
+
+                        transposeX = gridData.GetLength(0) - 1 - transposeX;
+                        
+                        SetEdgeData(transposeX, transposeY);
+                    }
+                }
+            }
+            
+            _signalBus.Fire(new DotCheckSignal());
+            _signalBus.Fire(new CheckFillAreaSignal());
+        }
+        
+        private void SetEdgeData(int x, int y)
+        {
+            GridEdge[,] edges = gridGenerator.Edges;
+            
+            edges[x,y].SetOccupied(Direction.Left);
+            edges[x,y].SetOccupied(Direction.Down);
+            
+            edges[x + 1,y].SetOccupied(Direction.Left);
+            edges[x,y + 1].SetOccupied(Direction.Down);
+            
+            edges[x,y].SetColor(Direction.Left, _gameProgressModel.ThemeColor);
+            edges[x,y].SetColor(Direction.Down, _gameProgressModel.ThemeColor);
+            
+            edges[x + 1, y].SetColor(Direction.Left, _gameProgressModel.ThemeColor);
+            edges[x,y + 1].SetColor(Direction.Down, _gameProgressModel.ThemeColor);
         }
         
         protected override void SubscribeToSignals()
